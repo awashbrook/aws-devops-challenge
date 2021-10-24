999115f5a977de437128df25a63dc412d2e8c61a
awashbrook <awashbrook@gmail.com>
2021-10-24 12:24:23 +0100

    Fixing outputs from VPC module

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,16 @@
-output "vpc_application_id" {
-  value = module.vpc_application.vpc_id
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc_application.vpc_id
 }
 output "internet_gateway_id" {
-  value = module.vpc_application.public_internet_gateway_route_id
+  description = "The ID of the Internet Gateway"
+  value       = module.vpc_application.igw_id
 }
 output "public_subnets_id" {
-  value = module.vpc_application.public_subnets
+  description = "List of IDs of public subnets"
+  value       = module.vpc_application.public_subnets
 }
 output "private_subnets_id" {
-  value = module.vpc_application.private_subnets
+  description = "List of IDs of private subnets"
+  value       = module.vpc_application.private_subnets
 }
